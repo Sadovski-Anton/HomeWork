326c25eb69a2d3b51ef06d72a081a250b2e6810b
Sadovski-Anton <111460197+Sadovski-Anton@users.noreply.github.com>
2022-08-18 00:03:44 +0300

    Add files via upload

---
+++ 3.4.py
@@ -0,0 +1,7 @@
+first = input('Ввведите первое число: ')
+sekond = input('Ввведите второе число: ')
+third = input('Ввведите третье число: ')
+
+print('Если False, то число положительное---', first.startswith('-'))
+print('Если False, то число положительное---', sekond.startswith('-'))
+print('Если False, то число положительное---', third.startswith('-'))

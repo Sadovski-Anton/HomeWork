22ff07c8590d6035a6a4c0bcbffa9cafe17b9aed
Sadovski-Anton <111460197+Sadovski-Anton@users.noreply.github.com>
2022-08-18 21:37:01 +0300

    Update 3.3.py

--- 3.3.py
+++ 3.3.py
@@ -2,6 +2,6 @@ name = input('Ввведите имя: ')
 age = input('Ввведите возраст: ')
 city = input('Ввведите город: ')
 
-print('Приветствуем тебя,', name, age, 'лет', 'из', city)
+print('Приветствуем тебя, %s %s лет из %s' %(name, age, city))
 print(f'Приветствуем тебя, {name} {age} лет из {city}')
 print('Приветствуем тебя, {} {} лет из {}'.format(name, age, city))

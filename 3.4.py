f8af2465a7949222400347c7032e59b1e4024659
Sadovski-Anton <111460197+Sadovski-Anton@users.noreply.github.com>
2022-08-18 21:47:47 +0300

    Update 3.4.py

--- 3.4.py
+++ 3.4.py
@@ -2,6 +2,11 @@ first = input('Ввведите первое число: ')
 sekond = input('Ввведите второе число: ')
 third = input('Ввведите третье число: ')
 
-print('Если False, то число положительное---', first.startswith('-'))
-print('Если False, то число положительное---', sekond.startswith('-'))
-print('Если False, то число положительное---', third.startswith('-'))
+first_new = first.startswith('-')
+sekond_new = sekond.startswith('-')
+third_new = third.startswith('-')
+
+result_pol = 3 - first_new - sekond_new - third_new
+result_otr = 3 - result_pol
+print(f'Пользователь ввел  положительных чисел: {result_pol}')
+print(f'Пользователь ввел  отрицательных чисел: {result_otr}')

e6ffbbed85ade0c41df27b7addacd45b19cfccd5
Sadovski-Anton <sad.k604@gmail.com>
2022-08-23 17:02:51 +0300

    Калькулятор

--- 5.2.py
+++ 5.2.py
@@ -1,12 +1,16 @@
 number_first: int = int(input('Введите первое число:'))
 ryx: str = input('Введите нужное математическое действие:')
 number_second: int = int(input('Введите второе число: '))
+
 if ryx == '+':
     rez = number_first + number_second
 elif ryx == '-':
     rez = number_first - number_second
 elif ryx == '/':
-    rez = number_first / number_second
+    if number_second == 0:
+        rez = False
+    else:
+        rez = number_first / number_second
 elif ryx == '*':
     rez = number_first * number_second
 else:
@@ -15,4 +19,4 @@ else:
 if rez:
     print(f'{number_first} {ryx} {number_second} = {rez}')
 else:
-    print('Введите корректное математическое действие!')
+    print('Введите корректное математическое действие или число!')

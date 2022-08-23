403dd1bc039d5eb97a5e010258b50771c786fa0a
Sadovski-Anton <sad.k604@gmail.com>
2022-08-23 10:41:48 +0300

    test

--- 4.3.py
+++ 4.3.py
@@ -1,4 +1,4 @@
 number = int(input('Введите число: '))
-result = dict((i, {'name': input('Введите имя: '), 'email': input('Введите почту: ')}) for i in range(number + 1))
+result = dict((i, {'name': input('Введите имя: '), 'email': input('Введите почту: ')}) for i in range(1, number + 1))
 
 print(result)

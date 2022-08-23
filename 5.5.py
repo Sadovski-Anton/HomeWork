7ba320e3dd0586449282d26902bac20d65a7f946
Sadovski-Anton <sad.k604@gmail.com>
2022-08-23 21:12:43 +0300

    Сортировка списка без встроенных методов

---
+++ 5.5.py
@@ -0,0 +1,12 @@
+num = [1, 3, 6, 2, 90, -5, 0, 0, 90]
+num_sort = []
+num_sort.append(num[0])
+for i in num[1:]:
+    if i <= num_sort[0]:
+        num_sort.insert(0, i)
+    else:
+        j = 0
+        while j < len(num_sort) and i > num_sort[j]:
+            j += 1
+        num_sort.insert(j, i)
+print(num_sort)

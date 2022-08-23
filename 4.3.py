5f02e1498781276e09f9c75272240959dbd82311
Sadovski-Anton <sad.k604@gmail.com>
2022-08-23 11:36:37 +0300

    test

--- 4.3.py
+++ 4.3.py
@@ -1,4 +1,6 @@
+from pprint import pprint
 number = int(input('Введите число: '))
 result = dict((i, {'name': input('Введите имя: '), 'email': input('Введите почту: ')}) for i in range(1, number + 1))
 
-print(result)
+pprint(result)
+

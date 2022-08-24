0000ac6568cce4f46720eb6fa5f4e7fa925f5758
Sadovski-Anton <sad.k604@gmail.com>
2022-08-24 11:27:34 +0300

    Треугольник Паскаля

---
+++ 5.7.py
@@ -0,0 +1,7 @@
+hith: int = int(input('Введите нужную глубину треугольника:'))
+stroka: list = []
+for i in range(1, hith + 1):
+    stroka.insert(0, 1)
+    for j in range(1, len(stroka) - 1):
+        stroka[j] += stroka[j+1]
+    print(stroka)

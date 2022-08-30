9936712ef3e9d9551455977ac2c9004366b5c4c4
Sadovski-Anton <sad.k604@gmail.com>
2022-08-30 17:04:23 +0300

    Дан список, вывести сумму соседних элементов

--- 6.7.py
+++ 6.7.py
@@ -1,14 +1,13 @@
 data: list = [1, 5, 7, 9, 3, 5, 4, 6]
 
 
-def sum_number(n: int):
+def sum_number(n: int)-> list:
     if n == len(data)-1:
-        rez = data[n-1] + data[0]
+        rez: int = data[n-1] + data[0]
     else:
-        rez = data[n-1] + data[n+1]
+        rez: int = data[n-1] + data[n+1]
     return rez
 
 
 result: list = list(map(lambda x: sum_number(data.index(x)), data))
 print(result)
- 

ab0b555a59c4ece2869012f3a2833c119335e060
Sadovski-Anton <sad.k604@gmail.com>
2022-08-30 17:13:47 +0300

    Сместить список на число N

--- 6.3.py
+++ 6.3.py
@@ -1,9 +1,15 @@
 data: list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-n: int = int(input('Введите число смещения чисел:'))
-c: int = 0
-while c < n:
-   a: int = data[0]
-   del data[0]
-   data.append(a)
-   c += 1
-print(data)
+
+
+def sdvig(data: list) -> list:
+   n: int = int(input('Введите число смещения чисел:'))
+   c: int = 0
+   while c < n:
+      a: int = data[0]
+      del data[0]
+      data.append(a)
+      c += 1
+   print(data)
+
+
+sdvig(data)

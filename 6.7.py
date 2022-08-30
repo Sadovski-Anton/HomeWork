78661acec7e0edd362a73dc7372deae65cf16b7f
Sadovski-Anton <sad.k604@gmail.com>
2022-08-30 12:04:40 +0300

    Сместить список на число N

--- 6.7.py
+++ 6.7.py
@@ -1,11 +1,13 @@
 data: list = [1, 5, 7, 9, 3, 5, 4, 6]
 
+
 def sum_number(n: int):
     if n == len(data)-1:
         rez = data[n-1] + data[0]
     else:
         rez = data[n-1] + data[n+1]
     return rez
+
 
 result: list = list(map(lambda x: sum_number(data.index(x)), data))
 print(result)

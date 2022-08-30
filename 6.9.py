17aa30135d84aa42165a4ee11936a073a3bc5b58
Sadovski-Anton <sad.k604@gmail.com>
2022-08-30 11:56:58 +0300

    Дан словарь словарей, вывести пользователей с отсутсвующими email

--- 6.9.py
+++ 6.9.py
@@ -1,6 +1,12 @@
-data: list = {
+data: dict = {
     1: {'name': 'Fedor', 'soname': 'Petrov', 'telefon': 123456789, 'email': 'info@gmail.com'},
     2: {'name': 'Fedor2', 'soname': 'Petrov2', 'telefon': 1234567892},
-    3: {'name': 'Fedor3', 'soname': 'Petrov3', 'telefon': 1234567893, 'email': ''}
+    3: {'name': 'Fedor3', 'soname': 'Petrov3', 'telefon': 1234567893, 'email': ''},
+    4: {'name': 'Fedor4', 'soname': 'Petrov4', 'telefon': 1234567894, '123123': 'info@gmail.com'}
 }
+select_posishion = 'email'
 
+for i in range(1, len(data)+1):
+    a = data.get(i)
+    if select_posishion not in a or a[select_posishion] == '':
+        print(a['name'])

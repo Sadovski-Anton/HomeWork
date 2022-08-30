947983b629a66594893c71154ff190ea701d075b
Sadovski-Anton <sad.k604@gmail.com>
2022-08-30 10:41:46 +0300

    Дан словарь словарей, вывести тех у кого нет почты, или значение - пустая строка

---
+++ 6.9.py
@@ -0,0 +1,6 @@
+data: list = {
+    1: {'name': 'Fedor', 'soname': 'Petrov', 'telefon': 123456789, 'email': 'info@gmail.com'},
+    2: {'name': 'Fedor2', 'soname': 'Petrov2', 'telefon': 1234567892},
+    3: {'name': 'Fedor3', 'soname': 'Petrov3', 'telefon': 1234567893, 'email': ''}
+}
+

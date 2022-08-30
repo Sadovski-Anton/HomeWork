ddf4da3b0cafc0515fe3d57d472c0181bcbdc14c
Sadovski-Anton <sad.k604@gmail.com>
2022-08-30 21:30:23 +0300

    Вариант 1. Перевод с десятичного в двоичное число

--- 6.1.1.py
+++ 6.1.1.py
@@ -1,9 +1,24 @@
+data: dict = {
+    '0': 0,
+    '1': 1,
+    '2': 2,
+}
+
+
 def is_dvoika()-> int:
-    number_10: int = int(input('Введите число для конвертации:'))
+    number_10: str = (input('Введите число для конвертации:'))
+    num_10: int = 0
+    x: int = 1
+
+    for i in number_10:
+        num_10 += data[i] * 10**(len(number_10) - x)
+        x += 1
+
     number_2: str = ''
-    while number_10 > 0:
-        number_2 = str(number_10 % 2) + number_2
-        number_10 = number_10 // 2
+
+    while num_10 > 0:
+        number_2 = str(num_10 % 2) + number_2
+        num_10 = num_10 // 2
     print(number_2)
 
 

cfc46f45708e29c30a3e7cfc5caff6cd4498ffdc
Sadovski-Anton <sad.k604@gmail.com>
2022-08-30 12:37:38 +0300

    Сместить список на число N

--- 6.9.py
+++ 6.9.py
@@ -4,7 +4,7 @@ data: dict = {
     3: {'name': 'Fedor3', 'soname': 'Petrov3', 'telefon': 1234567893, 'email': ''},
     4: {'name': 'Fedor4', 'soname': 'Petrov4', 'telefon': 1234567894, '123123': 'info@gmail.com'}
 }
-select_posishion = 'email'
+select_posishion: str = 'email'
 
 for i in range(1, len(data)+1):
     a = data.get(i)

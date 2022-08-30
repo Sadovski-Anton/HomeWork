82a08f63c18982f4ef64366a1a211cf6a99cf7a7
Sadovski-Anton <sad.k604@gmail.com>
2022-08-30 17:07:33 +0300

    Дан список, отсортировать сначала четные, потом нечетные числа

--- 6.6.py
+++ 6.6.py
@@ -1,3 +1,9 @@
 data: list = [1, 4, 7, 2, 90, 356, 76, 0, 8]
-data: list = list(filter((lambda x: not x % 2), data)) + list(filter((lambda x: x % 2), data))
-print(data)
+
+
+def new_list(num: list) -> list:
+    num: list = list(filter((lambda x: not x % 2), num)) + list(filter((lambda x: x % 2), num))
+    print(num)
+
+
+new_list(data)

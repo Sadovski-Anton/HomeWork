ae67eb7703d1b80bc814356ba28eee83c16eb89c
Sadovski-Anton <sad.k604@gmail.com>
2022-08-30 12:39:19 +0300

    Вывод названия страны из словаря по значению города

--- 6.8.py
+++ 6.8.py
@@ -1,11 +1,11 @@
-data = {
+data: dict = {
     'Belarus': ['Минск', 'Гомель'],
     'Italy': ['Милан', 'Турин']
 }
 
 
-def sity_name():
-    name = input('Введите название города:')
+def sity_name() -> str:
+    name: str = input('Введите название города:')
     for i in data.keys():
         if name in data.get(i):
             print(i)

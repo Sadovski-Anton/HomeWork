be62da245d1bf17048d42838a4b4c70d7e430b59
Sadovski-Anton <sad.k604@gmail.com>
2022-08-31 23:15:03 +0300

    Дан словарь словарей, вывести тех у кого нет почты, или значение - пустая строка

--- 6.9.py
+++ 6.9.py
@@ -1,18 +1,15 @@
+select_posishion: str = 'email'
 data: dict = {
+    3: {'name': 'Fedor3', 'soname': 'Petrov3', 'telefon': 1234567893, 'email': ''},
     1: {'name': 'Fedor', 'soname': 'Petrov', 'telefon': 123456789, 'email': 'info@gmail.com'},
     2: {'name': 'Fedor2', 'soname': 'Petrov2', 'telefon': 1234567892},
-    3: {'name': 'Fedor3', 'soname': 'Petrov3', 'telefon': 1234567893, 'email': ''},
     4: {'name': 'Fedor4', 'soname': 'Petrov4', 'telefon': 1234567894, '123123': 'info@gmail.com'}
 }
-select_posishion: str = 'email'
-
-
 def proverka(number: str) -> str:
-    for i in range(1, len(data)+1):
+    for i in data.keys():
         a = data.get(i)
         if number not in a or a[number] == '':
             print(a['name'])
-
 
 
 proverka(select_posishion)
